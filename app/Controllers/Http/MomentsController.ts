c2da3169355d7bd06f8dd6c687520dd2c695a49f
Lucas Silva <lucas.goliveirass@gmail.com>
2024-03-26 14:59:44 -0300

    Revendo API e comentando

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -7,25 +7,25 @@ import Moment from 'App/Models/Moment';
 import Application from '@ioc:Adonis/Core/Application';
 
 export default class MomentsController {
-    private validationOptions = {
+    private validationOptions = { // Valida a imagem antes de inserir no sistema
         types: ['image'],
         size: '2mb',
     }
 
 
-    public async store({request, response}: HttpContextContract) {
+    public async store({request, response}: HttpContextContract) { // Cria um registro
         const body = request.body()
 
         const image = request.file('image', this.validationOptions)
 
         if(image) {
-            const imageName = `${uuidv4()}.${image.extname}`
+            const imageName = `${uuidv4()}.${image.extname}` // Concatena a extenção ao nome da imagem para que cada uma tenha um nome único
 
-            await image.move(Application.tmpPath('uploads'), {
+            await image.move(Application.tmpPath('uploads'), { // Armazena a imagem em tmpPath
                 name: imageName
             })
 
-            body.image = imageName
+            body.image = imageName // Salva a imagem com o nome atualizado no corpo da requisição
         }
 
         const moment = await Moment.create(body)
@@ -38,7 +38,7 @@ export default class MomentsController {
         }
     }
 
-    public async index() {
+    public async index() { // Busca todos os registros
         const moments = await Moment.query().preload('comments')
 
         return {
@@ -46,7 +46,7 @@ export default class MomentsController {
         }
     }
 
-    public async show({params}: HttpContextContract) {
+    public async show({params}: HttpContextContract) { // Busca um resgistro por id
         const moment = await Moment.findOrFail(params.id)
 
         await moment.load('comments')
@@ -77,7 +77,7 @@ export default class MomentsController {
 
         if(moment.image != body.image || !moment.image) {
             const image = request.file('image', this.validationOptions)
-            
+
             if (image) {
             const imageName = `${uuidv4()}.${image.extname}`
 
